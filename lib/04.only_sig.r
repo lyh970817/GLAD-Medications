4a8557739f68c7c189ed524167da982cc62d2fb7
Yuhao Lin <lyh970817@yandex.com>
2022-11-16 13:04:18 +0000

    Add only_sig parameter to discard rows with no sigs or set to NAs for plotting

--- lib/04.only_sig.r
+++ lib/04.only_sig.r
@@ -1,14 +1,27 @@
-only_sig <- function(models) {
+only_sig <- function(models, discard_row = TRUE) {
   # Keep only the significant variables
   map(models, function(model_indeps) {
     map(model_indeps, function(model) {
       p_cols <- grep("^p", colnames(model))
-      sig_rows <- apply(model[p_cols], 1, function(pvals) {
-        any(pvals <= 0.05)
-      }) %>%
-        which()
 
-      model[sig_rows, ]
+      if (discard_row) {
+        sig_rows <- apply(model[p_cols], 1, function(pvals) {
+          any(pvals <= 0.05)
+        }) %>%
+          which()
+
+        model <- model[sig_rows, ]
+      } else {
+        ci_cols <- grep("^CI", colnames(model))
+        coef_cols <- grep("^Coefficient", colnames(model))
+
+        insig_lgl <- model[p_cols] > 0.05
+        model[p_cols][insig_lgl] <- NA
+        model[ci_cols][rep_cols(insig_lgl, n = 2)] <- NA
+        model[coef_cols][insig_lgl] <- NA
+      }
+
+      model
     })
   })
 }
