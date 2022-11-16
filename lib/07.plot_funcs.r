66ac919a351ab05605a8ecdd8ccf9802417da43a
Yuhao Lin <lyh970817@yandex.com>
2022-11-16 13:06:59 +0000

    Combine multiple outcomes into one plot

--- lib/07.plot_funcs.r
+++ lib/07.plot_funcs.r
@@ -1,45 +1,60 @@
 require(forestploter)
 require(gridExtra)
-plot_hurdle <- function(model_plot) {
-  tm <- forest_theme(
-    base_size = 10,
-    refline_lty = "dashed",
-    ci_pch = c(15, 18),
-    ci_col = c("#377eb8", "#4daf4a"),
-    footnote_col = "blue",
-    legend_name = "Model",
-    legend_position = "bottom",
-    legend_value = c("Logistic", "Gamma"),
-    vertline_lty = c("dashed", "dotted"),
-    vertline_col = c("#d6604d", "#bababa"),
-    ref_line = 1,
-  )
+plot_models <- function(models, theme) {
+  models_plot <- map(models, function(models_indeps) {
+    model_indeps_plot <- map(
+      models_indeps[-1], function(model) {
+        # sample_size <- attr(model, "n")
+
+        model_plot <- model %>%
+          filter(!Parameter %in% labels[compete_indeps]) %>%
+          # mutate(n = paste(sample_size, collapse = ", ")) %>%
+          mutate(Parameter = str_remove(Parameter, "v\\.s.*$"))
+
+        model_plot
+      }
+    ) %>%
+      bind_rows(models_indeps[[1]], .)
+
+
+    model_indeps_plot
+  })
+
+
+  get_plot_value_list <- function(models_plot, which) {
+    map(models_plot, function(model_plot) {
+      select(model_plot, matches(paste0("^", which),
+        ignore.case = FALSE
+      )) %>%
+        as.list()
+    }) %>%
+      list.flatten()
+  }
+
+
+  coefficients_list <- get_plot_value_list(models_plot, which = "Coefficient")
+  p_list <- get_plot_value_list(models_plot, which = "p")
+  ci_high_list <- get_plot_value_list(models_plot, which = "CI_high")
+  ci_low_list <- get_plot_value_list(models_plot, which = "CI_low")
 
   xlim <- c(
-    min(c(model_plot$CI_low_logistic, model_plot$CI_low_gamma), na.rm = T),
-    max(c(model_plot$CI_high_logistic, model_plot$CI_high_gamma), na.rm = T)
+    min(unlist(ci_low_list), na.rm = T),
+    max(unlist(ci_high_list), na.rm = T)
   ) +
     c(-0.2, 0.2)
 
-  forest(model_plot[c("Parameter", " ")],
-    est = list(
-      model_plot$Coefficient_logistic,
-      model_plot$Coefficient_gamma
-    ),
-    lower = list(
-      model_plot$CI_low_logistic,
-      model_plot$CI_low_gamma
-    ),
-    upper = list(
-      model_plot$CI_high_logistic,
-      model_plot$CI_high_gamma
-    ),
-    sizes = 0.8,
-    xlim = xlim,
-    title = labels[attr(model_plot, "dep")],
-    ci_column = 2,
-    ref_line = 1,
-    theme = tm
+  forest(tibble(
+    Parameter = models_plot[[1]]$Parameter,
+    ` ` = paste(rep(" ", 40), collapse = " ")
+  ),
+  est = coefficients_list,
+  lower = ci_low_list,
+  upper = ci_high_list,
+  sizes = 0.8,
+  xlim = xlim,
+  ci_column = 2,
+  ref_line = 1,
+  theme = theme
   )
 }
 
